7e08be6f2018c6cb34fbcbef1db2f1c09ca64349
MuratYvs <muratyvs99@gmail.com>
2024-03-14 12:11:15 +0300

    Update header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,14 @@
+import { NavLink } from "react-router-dom";
 import styles from "./Header.module.css";
 
 const Header = () => {
   return (
     <div className={styles["main-nav"]}>
-      <div className={styles["site-logo"]}>Habit Tracker</div>
+      <div className={styles["site-logo"]}>
+        <NavLink to="./" className={styles["site-name"]}>
+          Habit Tracker
+        </NavLink>
+      </div>
 
       <div className={styles["user-session"]}>Sign In / Sign Up</div>
     </div>
